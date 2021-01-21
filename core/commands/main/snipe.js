7ecdb44cf7d1aa8489d772a9807a23b3cfe658f0
zirconiium <63737942+zirconiium@users.noreply.github.com>
2021-01-21 16:16:50 +0100

    fixed unlimted snipe bug

--- core/commands/main/snipe.js
+++ core/commands/main/snipe.js
@@ -3,6 +3,8 @@ const db = require('quick.db');
 
 exports.launch = (client, message, args) => {
   const snipes = db.get(`snipes_${args[0] == "edit" ? "edited" : "deleted"}_${message.channel.id}`);
+  if (!snipes) return;
+
   const embed = new MessageEmbed()
     .setColor(0x2F3136)
     .setAuthor(snipes.username, snipes.avatarURL)
@@ -11,7 +13,7 @@ exports.launch = (client, message, args) => {
     .setFooter("gokium", client.user.displayAvatarURL({format: "png" || "gif"}));
 
   if (snipes.newMessage) embed.addField("**Nouveau Message**", snipes.newMessage, true);
-  return message.channel.send(embed);
+  return message.channel.send(embed).then(() => db.set(`snipes_${args[0] == "edit" ? "edited" : "deleted"}_${message.channel.id}`, null));
 }
 
 exports.commands = {

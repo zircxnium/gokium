d83087387102d20bb81a4af8ee62b580a68f5ca6
zirconiium <63737942+zirconiium@users.noreply.github.com>
2021-03-07 21:45:47 +0100

    [event] inviteDelete typo

--- core/events/inviteDelete.js
+++ core/events/inviteDelete.js
@@ -4,7 +4,7 @@ const { invites } = require('./ready');
 exports.run = async (client, invite) => {
     if (invite.guild.me.hasPermission("ADMINISTRATOR") || invite.guild.me.hasPermission("VIEW_AUDIT_LOG")) {
         if (!invites[invite.guild.id]) return;
-        delete invites[invite.guild.id][inviteode];
+        delete invites[invite.guild.id][invite.code];
 
         // logs ?
     }

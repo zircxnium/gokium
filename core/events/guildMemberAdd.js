962888ea12cdc01a3c553be9539e65c56dfbc696
zirconiium <63737942+zirconiium@users.noreply.github.com>
2021-03-07 16:57:29 +0100

    [guildMemberAdd] can now fetch vanity url/custom invite url

--- core/events/guildMemberAdd.js
+++ core/events/guildMemberAdd.js
@@ -1,22 +1,30 @@
 const utils = require('../utils');
 const { invites } = require('./ready');
 
-exports.run = (client, member) => {
+exports.run = async (client, member) => {
     if (member.guild.me.hasPermission("ADMINISTRATOR") || member.guild.me.hasPermission("VIEW_AUDIT_LOG")) {
-        member.guild.fetchInvites().then(guildInvites => {
+        await member.guild.fetchInvites()
+        .then(async guildInvites => {
             // This is the *existing* invites for the guild.
             const ei = invites[member.guild.id];
             // Update the cached invites for the guild.
             invites[member.guild.id] = guildInvites;
             // Look through the invites, find the one for which the uses went up.
-            const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+            let invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
             // This is just to simplify the message being sent below (inviter doesn't have a tag property)
-            const inviter = client.users.cache.get(invite.inviter.id);
+            const inviter = invite && client.users.cache.get(invite.inviter.id).id;
+
+            // if invite == null, most likely cause it's a custom invite.
+            if (!invite) {
+                // Fetch invite data
+                await member.guild.fetchVanityData()
+                .then(res => invite = res);
+            }
 
             utils.logs({
                 client,
                 title: "Quelqu'un à rejoint le serveur 😮",
-                message: `🙎‍♂️ **<@${member.id}>** à rejoint le serveur avec l'invite https://discord.gg/${invite.code} de <@${inviter.id}>\nInvitation utilisée **${invite.uses}x** depuis sa création`,
+                message: `🙎‍♂️ **<@${member.id}>** à rejoint le serveur avec l'invite https://discord.gg/${(invite.code)} ${inviter && `de <@${inviter}>` || ""}\nInvitation utilisée **${invite.uses}x** depuis sa création`,
                 guildId: member.guild.id,
                 initiator: member
             });

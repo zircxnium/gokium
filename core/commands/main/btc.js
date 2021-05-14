aa1971854ddd1866e066dd0f6cc65bfc62a3de72
zirconiium <63737942+zirconiium@users.noreply.github.com>
2021-05-14 14:58:30 +0200

    added btc command

---
+++ core/commands/main/btc.js
@@ -0,0 +1,25 @@
+const currencyCodes = ["AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BOV", "BRL", "BSD", "BTN", "BWP", "BYR", "BZD", "CAD", "CDF", "CHE", "CHF", "CHW", "CLF", "CLP", "CNY", "COP", "COU", "CRC", "CUC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "EUR", "FJD", "FKP", "GBP", "GEL", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF", "IDR", "ILS", "INR", "IQD", "IRR", "ISK", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KMF", "KPW", "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LTL", "LVL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRO", "MUR", "MVR", "MWK", "MXN", "MXV", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SRD", "SSP", "STD", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TWD", "TZS", "UAH", "UGX", "USD", "USN", "USS", "UYI", "UYU", "UZS", "VEF", "VND", "VUV", "WST", "XAF", "XAG", "XAU", "XBA", "XBB", "XBC", "XBD", "XCD", "XDR", "XFU", "XOF", "XPD", "XPF", "XPT", "XTS", "XXX", "YER", "ZAR", "ZMW"];
+const { MessageEmbed } = require("discord.js");
+const fetch = require('node-fetch');
+
+exports.launch = (client, message, args, lang) => {
+    args[0] = args[0].toUpperCase();
+
+    if (!args[0] || !currencyCodes.find(code => args[0] === code)) return;
+    
+    const embed = new MessageEmbed()
+        .setColor(0x2F3136)
+        .setTitle("Bitcoin Price")
+        .setTimestamp()
+        .setFooter("gokium", client.user.displayAvatarURL({format: "png" || "gif"}));
+        
+    fetch(`https://api.nomics.com/v1/currencies/ticker?key=0d0441f5c761182e7d49a5fc5a0dacdb&ids=BTC&interval=1h&convert=${args[0]}&per-page=100&page=1`)
+    .then(response => response.json())
+    .then(body => {
+        if (!body) return;
+        
+        const price = parseFloat(body[0].price);
+        embed.addField(`**${args[0]}**`, price.toFixed(2));
+        message.channel.send(embed);
+    })
+}

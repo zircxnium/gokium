687879bec87c5e56c6c3ec9455f09959c37f49ae
zirconiium <63737942+zirconiium@users.noreply.github.com>
2021-05-14 14:58:23 +0200

    fixed bot invite command

--- core/commands/main/botinvite.js
+++ core/commands/main/botinvite.js
@@ -1,3 +1,3 @@
 exports.launch = (client, message, args, lang) => {
-  return message.reply(lang.replyMsg).then(() => message.author.send(`https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&permissions=8&scope=bot`));
+  return message.reply(lang.replyMsg).then(() => message.author.send(`https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot`));
 }
